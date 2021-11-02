109b67072095c0aab4d7834b6e43e6a902c4e752
TaewooKim <embedded61@gmail.com>
2021-11-02 21:05:12 +0900

    Add: filter 추가 및 버그 수정

--- posts/views.py
+++ posts/views.py
@@ -1,14 +1,15 @@
-import json, re, bcrypt, jwt 
-from json              import JSONDecodeError
+import json, re, bcrypt, jwt
+from json                 import JSONDecodeError
 
-from django.views      import View
-from django.http       import JsonResponse
-from django.shortcuts  import get_object_or_404
-from django.db.models  import F
+from django.views         import View
+from django.http          import JsonResponse
+from django.http.response import Http404 
+from django.shortcuts     import get_object_or_404
+from django.db.models     import F, Q
 
-from posts.models      import Post, Comment, Category
-from users.models      import User, AccessLog
-from users.decorators  import login_decorator
+from posts.models         import Post, Comment, Category
+from users.models         import User, AccessLog
+from users.decorators     import login_decorator
 
 
 class CommentView(View):
@@ -72,7 +73,6 @@ class CommentView(View):
         comment.delete()
         return JsonResponse({'MESSAGE' : 'COMMENT_DELETED'}, status=204)
 
-
 class PostListCreateView(View):
     @login_decorator
     def post(self, request):
@@ -86,7 +86,7 @@ class PostListCreateView(View):
             if data['content'] == '':
                 return JsonResponse({'MESSAGE': 'CONTENT_ERROR'}, status=400)
 
-            category_id = int(data.get('category_id', 0))
+            category_id = data.get('category_id', 0)
             if not Category.objects.filter(id=category_id).exists():
                 return JsonResponse({'MESSAGE': 'CATEGORY_ERROR'}, status=400)
 
@@ -104,28 +104,52 @@ class PostListCreateView(View):
 
     @login_decorator
     def get(self, request):
-        limit  = int(request.GET.get('limit','20'))
-        offset = int(request.GET.get('offset','0'))
-        offset = offset * limit
 
-        post_count = Post.objects.all().count()
-        posts = Post.objects.all()[offset:offset+limit]
+        order_conditions = {
+            'new'       : '-created_at',
+            'old'       : 'created_at',
+            'high_view' : '-viewcount',
+            'low_view'  : 'viewcount',
+        }
+
+        category = request.GET.get('category')
+        subject  = request.GET.get('subject')
+        order    = request.GET.get('order', '')
+        limit    = int(request.GET.get('limit','20'))
+        offset   = int(request.GET.get('offset','0'))
+        offset   = offset * limit
+        q        = Q()
         
+        if category:
+            q &= Q(category__name=category)
+
+        if subject:
+            q &= Q(subject__icontains=subject)
+
+        filtered_posts = Post.objects.filter(q) \
+            .select_related('category', 'user') \
+            .order_by(order_conditions.get(order, 'id'))
+
+        post_count = filtered_posts.count()
+    
         posts_list = [{
-            'user'   : post.user.name,
-            'subject' : post.subject,
-            'content' : post.content,
-            'category' : post.category.name,
+            'id'         : post.id,
+            'user'       : post.user.name,
+            'subject'    : post.subject,
+            'content'    : post.content,
+            'category_id': post.category_id,
+            'category'   : post.category.name,
             'view_count' : post.viewcount,
             'created_at' : post.created_at.strftime('%Y-%m-%d %H:%M:%S'),
             'updated_at' : post.updated_at.strftime('%Y-%m-%d %H:%M:%S'),
-        } for post in posts]
+        } for post in filtered_posts]
 
         result = {
-            'count': len(post_count),
+            'count': post_count,
             'data' : posts_list
         }
         return JsonResponse({'results': result}, status=200)
+
 
 class PostRetrieveDeleteEditView(View):
     @login_decorator
@@ -135,18 +159,21 @@ class PostRetrieveDeleteEditView(View):
             post = get_object_or_404(Post, id=post_id)
             _, is_exsist = AccessLog.objects.get_or_create(post_id=post_id, user_id=user_id)
 
-            if not is_exsist:
-                post.viewcount = F('viewcount') + 1
+            if is_exsist:
+                post.viewcount = post.viewcount + 1
                 post.save()
+                post.refresh_from_db()
             
             post_data = {
-                'user'   : post.user.name,
-                'subject' : post.subject,
-                'content' : post.content,
-                'category' : post.category.name,
-                'view_count' : post.viewcount,
-                'created_at' : post.created_at.strftime('%Y-%m-%d %H:%M:%S'),
-                'updated_at' : post.updated_at.strftime('%Y-%m-%d %H:%M:%S'),
+                'id'           : post.id,
+                'user'         : post.user.name,
+                'subject'      : post.subject,
+                'content'      : post.content,
+                'category_id'  : post.category_id,
+                'category_name': post.category.name,
+                'view_count'   : post.viewcount,
+                'created_at'   : post.created_at.strftime('%Y-%m-%d %H:%M:%S'),
+                'updated_at'   : post.updated_at.strftime('%Y-%m-%d %H:%M:%S'),
             }
 
             result = {
@@ -155,7 +182,7 @@ class PostRetrieveDeleteEditView(View):
 
             return JsonResponse({'results': result}, status=200)
     
-        except Post.DoesNotExist:
+        except Http404:
             return JsonResponse({'MESSAGE': 'POST_DOSE_NOT_EXIST'}, status=404)
 
     @login_decorator
@@ -163,6 +190,9 @@ class PostRetrieveDeleteEditView(View):
         try:
             data = json.loads(request.body)
             post = get_object_or_404(Post, id=post_id)
+
+            if not request.user.id == post.user.id:
+                return JsonResponse({'MESSAGE': 'INVALID_USER'}, status=403)
             
             if data['subject'] == '':
                 return JsonResponse({'MESSAGE': 'SUBJECT_ERROR'}, status=400)
@@ -170,8 +200,8 @@ class PostRetrieveDeleteEditView(View):
             if data['content'] == '':
                 return JsonResponse({'MESSAGE': 'CONTENT_ERROR'}, status=400)
 
-            category_id = int(data['category'], -1)
-            if not Category.objects.filter(category_id=category_id).exists():
+            category_id = data.get('category_id', 0)
+            if not Category.objects.filter(id=category_id).exists():
                 return JsonResponse({'MESSAGE': 'CATEGORY_ERROR'}, status=400)
 
             post.subject = data['subject']
@@ -181,7 +211,7 @@ class PostRetrieveDeleteEditView(View):
 
             return JsonResponse({'MESSAGE': 'SUCCESS'}, status=201)
 
-        except Post.DoesNotExist:
+        except Http404:
             return JsonResponse({'MESSAGE': 'POST_DOSE_NOT_EXIST'}, status=404)
 
         except KeyError:
@@ -198,5 +228,5 @@ class PostRetrieveDeleteEditView(View):
             post.delete()
             return JsonResponse({'MESSAGE': 'NO_CONTENT'}, status=204)
 
-        except Post.DoesNotExist:
+        except Http404:
             return JsonResponse({'MESSAGE': 'POST_DOSE_NOT_EXIST'}, status=404)
