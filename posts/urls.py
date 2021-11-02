24145879392597004ccaf02e29fdd3d2f83cdf3c
TaewooKim <embedded61@gmail.com>
2021-11-02 21:05:12 +0900

    Add: filter 추가 및 버그 수정

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views      import PostListCreateView, CommentView
+from .views      import PostListCreateView, PostRetrieveDeleteEditView, CommentView
 
 urlpatterns = [
     path("", PostListCreateView.as_view()),
+    path('/<int:post_id>', PostRetrieveDeleteEditView.as_view()),
     path("/<int:post_id>/comments", CommentView.as_view())
 ]
