ba1bc76ab3d580c0c0a90d653db626bf8af3621f
TaewooKim <embedded61@gmail.com>
2021-11-02 18:24:02 +0900

    Add: 게시물 CRUD 구현

--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,15 @@
 import json, re, bcrypt, jwt 
+from json              import JSONDecodeError
 
-from django.views            import View
-from django.http             import JsonResponse
+from django.views      import View
+from django.http       import JsonResponse
+from django.shortcuts  import get_object_or_404
+from django.db.models  import F
 
-from posts.models            import Post, Comment
-from users.models            import User
-from users.utils             import login_decorator
+from posts.models      import Post, Comment, Category
+from users.models      import User, AccessLog
+from users.decorators  import login_decorator
+
 
 class CommentView(View):
     @login_decorator
@@ -27,9 +31,6 @@ class CommentView(View):
         try:
             data = json.loads(request.body)
             user = request.user
-
-            if not (content and post_id):
-                return JsonResponse({'message':'KEY_ERROR'}, status=400)
 
             if not Post.objects.filter(id=post_id).exists():
                 return JsonResponse({'message':'POST_DOES_NOT_EXIST'}, status=404)
@@ -70,3 +71,132 @@ class CommentView(View):
 
         comment.delete()
         return JsonResponse({'MESSAGE' : 'COMMENT_DELETED'}, status=204)
+
+
+class PostListCreateView(View):
+    @login_decorator
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+            user_id = request.user.id
+            
+            if data['subject'] == '':
+                return JsonResponse({'MESSAGE': 'SUBJECT_ERROR'}, status=400)
+            
+            if data['content'] == '':
+                return JsonResponse({'MESSAGE': 'CONTENT_ERROR'}, status=400)
+
+            category_id = int(data['category'], -1)
+            if not Category.objects.filter(category_id=category_id).exists():
+                return JsonResponse({'MESSAGE': 'CATEGORY_ERROR'}, status=400)
+
+            Post.objects.create(
+                    user     = User.objects.get(id=user_id),
+                    subject  = data['subject'],
+                    content  = data['content'],
+                    category_id = category_id
+                )
+
+            return JsonResponse({'MESSAGE': 'SUCCESS'}, status=201)
+
+        except KeyError:
+            return JsonResponse({'MESSAGE': 'KEY_ERROR'}, status=400)
+
+    @login_decorator
+    def get(self, request):
+        limit  = int(request.GET.get('limit','20'))
+        offset = int(request.GET.get('offset','0'))
+        offset = offset * limit
+
+        post_count = Post.objects.all().count()
+        posts = Post.objects.all()[offset:offset+limit]
+        
+        posts_list = [{
+            'user'   : post.user.name,
+            'subject' : post.subject,
+            'content' : post.content,
+            'category' : post.category.name,
+            'view_count' : post.viewcount,
+            'created_at' : post.created_at.strftime('%Y-%m-%d %H:%M:%S'),
+            'updated_at' : post.updated_at.strftime('%Y-%m-%d %H:%M:%S'),
+        } for post in posts]
+
+        result = {
+            'count': len(post_count),
+            'data' : posts_list
+        }
+        return JsonResponse({'results': result}, status=200)
+
+class PostRetrieveDeleteEditView(View):
+    @login_decorator
+    def get(self, request, post_id):
+        try:
+            user_id = request.user.id
+            post = get_object_or_404(Post, id=post_id)
+            _, is_exsist = AccessLog.objects.get_or_create(post_id=post_id, user_id=user_id)
+
+            if not is_exsist:
+                post.viewcount = F('viewcount') + 1
+                post.save()
+            
+            post_data = {
+                'user'   : post.user.name,
+                'subject' : post.subject,
+                'content' : post.content,
+                'category' : post.category.name,
+                'view_count' : post.viewcount,
+                'created_at' : post.created_at.strftime('%Y-%m-%d %H:%M:%S'),
+                'updated_at' : post.updated_at.strftime('%Y-%m-%d %H:%M:%S'),
+            }
+
+            result = {
+                'data' : post_data
+            }
+
+            return JsonResponse({'results': result}, status=200)
+    
+        except Post.DoesNotExist:
+            return JsonResponse({'MESSAGE': 'POST_DOSE_NOT_EXIST'}, status=404)
+
+    @login_decorator
+    def put(self, request, post_id):
+        try:
+            data = json.loads(request.body)
+            post = get_object_or_404(Post, id=post_id)
+            
+            if data['subject'] == '':
+                return JsonResponse({'MESSAGE': 'SUBJECT_ERROR'}, status=400)
+            
+            if data['content'] == '':
+                return JsonResponse({'MESSAGE': 'CONTENT_ERROR'}, status=400)
+
+            category_id = int(data['category'], -1)
+            if not Category.objects.filter(category_id=category_id).exists():
+                return JsonResponse({'MESSAGE': 'CATEGORY_ERROR'}, status=400)
+
+            post.subject = data['subject']
+            post.content = data['content']
+            post.category_id = category_id
+            post.save()
+
+            return JsonResponse({'MESSAGE': 'SUCCESS'}, status=201)
+
+        except Post.DoesNotExist:
+            return JsonResponse({'MESSAGE': 'POST_DOSE_NOT_EXIST'}, status=404)
+
+        except KeyError:
+            return JsonResponse({'MESSAGE': 'KEY_ERROR'}, status=400)
+
+    @login_decorator
+    def delete(self, request, post_id):
+        try:
+            post = get_object_or_404(Post, id=post_id)
+
+            if not request.user.id == post.user.id:
+                    return JsonResponse({'MESSAGE': 'INVALID_USER'}, status=403)
+            
+            post.delete()
+            return JsonResponse({'MESSAGE': 'NO_CONTENT'}, status=204)
+
+        except Post.DoesNotExist:
+            return JsonResponse({'MESSAGE': 'POST_DOSE_NOT_EXIST'}, status=404)
